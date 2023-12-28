d2a26b04761a1258753017e5d98b14798b53a5e4
Kelvin Lee <lijiarong2020@sinh.ac.cn>
2023-12-28 21:51:33 +0800

    Update Seurat2Scanpy.py

--- Seurat2Scanpy/Seurat2Scanpy.py
+++ Seurat2Scanpy/Seurat2Scanpy.py
@@ -66,11 +66,42 @@ def st_Seurat2Scanpy(seurat_file,r_home,
     return(seurat_obj@images)
     """)
 
-    GetCoordinates=r("""
+    GetCoordinates_Visium=r("""
     function(image_obj){
     library(Seurat)
     coord_mat=image_obj@coordinates
     return(coord_mat[,c('imagerow','imagecol')])
+    }
+    """)
+    
+    GetCoordinates_SlideSeq=r("""
+    function(image_obj){
+    library(Seurat)
+    coord_mat=image_obj@coordinates
+    return(coord_mat[,c('x','y')])
+    }
+    """)
+    
+    GetCellCoordinates=r("""
+    function(image_obj){
+    library(Seurat)
+    return(GetTissueCoordinates(image_obj[["centroids"]])[,c('x','y')])
+    }
+    """)
+    
+    GetSegmentationCoordinates=r("""
+    function(image_obj){
+    library(Seurat)
+    return(GetTissueCoordinates(image_obj[["segmentation"]]))
+    }
+    """)
+    
+    GetMoleculeCoordinates=r("""
+    function(image_obj,assay,slot){
+    library(Seurat)
+    library(dplyr)
+    x=rownames(GetAssayData(seurat_obj,assay=assay,slot=slot))
+    return(GetTissueCoordinates(image_obj[["molecules"]]) %>% filter(molecule %in% x))
     }
     """)
 
@@ -82,7 +113,6 @@ def st_Seurat2Scanpy(seurat_file,r_home,
     return(coord_mat)
     }
     """)
-
 
     GetImage=r("""
     function(image_obj){
@@ -97,10 +127,7 @@ def st_Seurat2Scanpy(seurat_file,r_home,
     return(image_obj@scale.factors)
     }
     """)
-    
-    
-    
-    
+
     import pandas as pd
     import numpy as np
     
@@ -120,57 +147,81 @@ def st_Seurat2Scanpy(seurat_file,r_home,
     # metadata
     metadata=GetMetaData(seurat_obj)
     
-    spatial_dir={}
+    # create anndata
+    adata=ad.AnnData(X=count_mat,var=metafeature,obs=metadata)  
     coord_mat_ls=[]
-    array_coord_mat_ls=[]
     
     images=GetSpatial(seurat_obj)
+    ImageType=GetImageType(images[0])[0]
     
-    for i in range(len(images)):
-
-        image_obj=images[i]
-
-        # sample name
-        sample_name=images.names[i]
-
-        # coordinate matrix
-        coord_mat=GetCoordinates(image_obj)
-        coord_mat=np.array(coord_mat)
-        coord_mat_ls.append(coord_mat)
-        array_coord_mat=GetArrayCoordinates(image_obj)
-        array_coord_mat_ls.append(array_coord_mat)
+    # 10X Visium
+    if ImageType=='VisiumV1':
+        array_coord_mat_ls=[]
+        spatial_dir={}
+        for i in range(len(images)):
+            image_obj=images[i]
+            # sample name
+            sample_name=images.names[i]
+            # coordinate matrix
+            coord_mat=GetCoordinates_Visium(image_obj)
+            coord_mat=np.array(coord_mat)
+            coord_mat_ls.append(coord_mat)
+            array_coord_mat=GetArrayCoordinates(image_obj)
+            array_coord_mat_ls.append(array_coord_mat)
+            # image array
+            image_array=SeuratObject.GetImage(image_obj,mode='raw')
+            # scale factor
+            scalefactors=GetScaleFactor(image_obj)
+            scalefactors={scalefactors.names[i]:scalefactors[i][0] for i in range(len(scalefactors))}
+            scalefactors={'fiducial_diameter_fullres':scalefactors['fiducial'],
+            'tissue_hires_scalef':scalefactors['hires'],
+            'tissue_lowres_scalef':scalefactors['lowres']}
+            scalefactors['spot_diameter_fullres']=0.6196*scalefactors['fiducial_diameter_fullres']
+            spatial_dir[sample_name]={'images':{res_type:image_array},
+                                      'scalefactors':scalefactors}
+        # add spatial data
+        adata.uns['spatial']=spatial_dir
+        if len(array_coord_mat_ls)==1:
+            array_coord_mat=array_coord_mat_ls[0]
+        else:
+            array_coord_mat=pd.concat(array_coord_mat_ls,axis=0)
+        adata.obs=pd.concat((adata.obs,array_coord_mat),axis=1)
+    
+    # Slide-Seq
+    if ImageType=='SlideSeq':
+        for i in range(len(images)):
+            image_obj=images[i]
+            # sample name
+            sample_name=images.names[i]
+            # coordinate matrix
+            coord_mat=GetCoordinates_SlideSeq(image_obj)
+            coord_mat=np.array(coord_mat)
+            coord_mat_ls.append(coord_mat)
+    
+    # STARmap
+    # if ImageType=='STARmap':
         
-        # image array
-        image_array=SeuratObject.GetImage(image_obj,mode='raw')
-
-        # scale factor
-        scalefactors=GetScaleFactor(image_obj)
-        scalefactors={scalefactors.names[i]:scalefactors[i][0] for i in range(len(scalefactors))}
-        scalefactors={'fiducial_diameter_fullres':scalefactors['fiducial'],
-        'tissue_hires_scalef':scalefactors['hires'],
-        'tissue_lowres_scalef':scalefactors['lowres']}
-        scalefactors['spot_diameter_fullres']=0.6196*scalefactors['fiducial_diameter_fullres']
-
-        spatial_dir[sample_name]={'images':{res_type:image_array},
-                                  'scalefactors':scalefactors}
-
-    # create anndata
-    adata=ad.AnnData(X=count_mat,var=metafeature,obs=metadata)
+        
+    # MERSCOPe, Xenium, Nanostring CosMx, CODEX, etc.
+    if ImageType=='FOV':
+        for i in range(len(images)):
+            image_obj=images[i]
+            # sample name
+            sample_name=images.names[i]
+            # coordinate matrix
+            coord_mat=GetCellCoordinates(image_obj)
+            coord_mat=np.array(coord_mat)
+            coord_mat_ls.append(coord_mat)
+            # image_data=GetMoleculeCoordinates(image_obj,assay,slot)
+            # segmentation_data=GetSegmentationCoordinates=(image_obj)      
     
-    adata.uns['spatial']=spatial_dir
+    # add coordination matrix
     if len(coord_mat_ls)==1:
         coord_mat=coord_mat_ls[0]
     else:
         coord_mat=np.concatenate(coord_mat_ls,axis=0)
     coord_mat=coord_mat[:,[1,0]]
     adata.obsm['spatial']=coord_mat
-    
-    if len(array_coord_mat_ls)==1:
-        coord_mat=array_coord_mat_ls[0]
-    else:
-        coord_mat=pd.concat(array_coord_mat_ls,axis=0)
-    adata.obs=pd.concat((adata.obs,coord_mat),axis=1)
-    
 
     return(adata)
 
