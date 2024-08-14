32d1d6cb0b6b685cca84d925f850bbda2f1dba8c
Kelvin Lee <lijiarong2020@sinh.ac.cn>
2024-08-14 12:51:06 +0800

    Update custom_fun.R

--- visulization/custom_fun.R
+++ visulization/custom_fun.R
@@ -1,1 +1,101 @@
+# Required packages: ggplot2, Hmisc, dendsort
 
+CorPlot=function(df,cor.method='pearson', # 'pearson', 'spearman'
+        size='p.value', # 'p.value', 'p.adj'
+        p.adj.method='fdr',
+        sig.level=0.05,
+        tri='whole', # 'whole', 'lower', 'upper'
+        sig.circle=TRUE){
+    
+    library(ggplot2)
+
+    # cor_result and p_matirx could be replaced
+    cor_result=Hmisc::rcorr(as.matrix(df),type=cor.method)
+    cor_matrix=cor_result$r
+    p_matrix=cor_result$P
+
+    # clustering 
+    hc=hclust(as.dist(1-cor_matrix),method='ward.D2')
+    hc=dendsort::dendsort(hc)
+    ord.order=hc[['labels']][hc[['order']]]
+    cor_matrix=cor_matrix[ord.order,ord.order]
+
+    if (tri=='lower'){
+        cor_matrix[upper.tri(cor_result$r,diag=FALSE)]=NA
+    }
+    if (tri=='upper'){
+        cor_matrix[lower.tri(cor_result$r,diag=FALSE)]=NA
+    }
+
+    test_result=reshape2::melt(cor_matrix,varnames=c('x','y'),value.name='Correlation')
+    test_result$p.value=apply(test_result,1,function(x) p_matrix[x[1],x[2]])
+    test_result$p.value[is.na(test_result$Correlation)]=NA
+    test_result$p.adj=p.adjust(test_result$p.value,method=p.adj.method)
+    test_result$sig=test_result[,size]<=sig.level
+    # test_result$sig[is.na(test_result$sig)]=FALSE
+    # test_result[test_result[,'x']==test_result[,'y'],'Correlation']=NA
+    test_result[test_result[,'x']==test_result[,'y'],'p.value']=0.8
+    test_result[test_result[,'x']==test_result[,'y'],'p.adj']=0.8
+
+    if (!sig.circle){
+        p=ggplot(test_result,aes_string(x='x',y='y',fill='Correlation',size=size))
+    }
+
+    if(sig.circle){
+        p=ggplot(test_result,aes_string(x='x',y='y',fill='Correlation',size=size,color='sig'))+
+          scale_color_manual(values=c('TRUE'='black','FALSE'='#FFFFFF00'))
+    }
+
+    p=p+
+      geom_point(shape=21)+
+      scale_fill_gradient2(low='blue',mid='white',high='red',na.value='#CFCFCF')+
+      scale_size(trans='reverse')+
+      theme_minimal()+ 
+      coord_fixed()+
+      theme(axis.title.x = element_blank(),
+            axis.title.y = element_blank())+
+      theme(panel.background = element_blank(), 
+            panel.grid.major = element_blank(),
+            panel.border = element_blank())+
+      ggtitle(stringr::str_to_title(paste0(cor.method,' correlation')))+
+      theme(plot.title=element_text(hjust=0.5))+
+      labs(color=paste0('Sig\ (',size,'<=',sig.level,')'))+
+      scale_size_continuous(range=c(6,0.2))
+
+    if (tri=='lower'){
+        p=p+
+          scale_y_discrete(position='right')+
+          theme(axis.text.x=element_text(angle=45,hjust=1))
+    }
+
+    if (tri=='upper'){
+        p=p+
+          scale_x_discrete(position='top')+
+          theme(axis.text.x=element_text(angle=45,hjust=0))
+    }
+
+    if (tri=='whole'){
+        p=p+
+          theme(axis.text.x=element_text(angle=45,hjust=1))
+    }
+
+    if (tri=='upper'){
+      n=colnames(cor_matrix)
+      p=p+geom_text(data=data.frame(x=1.5:(length(n)+0.5),y=1:(length(n)+0),label=n),
+                    aes(x=x,y=y,label=label),
+                    angle=0, # 0/-45
+                    inherit.aes=FALSE,hjust=0)+
+      theme(axis.text.x=element_blank(),axis.text.y=element_blank())
+    }
+
+    if (tri=='lower'){
+      n=colnames(cor_matrix)
+      p=p+geom_text(data=data.frame(x=0.5:(length(n)-0.5),y=1:(length(n)+0),label=n),
+                    aes(x=x,y=y,label=label),
+                    angle=0, # 0/-45
+                    inherit.aes=FALSE,hjust=1)+
+      theme(axis.text.x=element_blank(),axis.text.y=element_blank())
+    }
+                              
+    return(p)
+}
