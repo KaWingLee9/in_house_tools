968be064c3eb3ee8d0668394e10f6b0399707922
Kelvin Lee <lijiarong2020@sinh.ac.cn>
2024-10-08 23:28:46 +0800

    Update custom_fun.R

--- meta_analysis/custom_fun.R
+++ meta_analysis/custom_fun.R
@@ -127,6 +127,7 @@ CombRank_DFLs=function(l,p_col='p_val',ES_col='log_fc',
     # min_num: minimal study number with test result
     # min_ratio: % of the results (ignoring NA) have consistent effect would be considered
     # method: "RankSum", "RankProd"
+    # ncores: number of cores to use
     
     gene_names=unique(unlist(lapply(l,rownames)))
     
@@ -137,10 +138,14 @@ CombRank_DFLs=function(l,p_col='p_val',ES_col='log_fc',
         return(x[,c('up_rank','down_rank')])
     },mc.cores=ncores)
     
-    test_result=sapply(gene_names,function(y){
-        ES=sapply(l,function(x){z=x[y,ES_col]})
+    test_result=parallel::mclapply(gene_names,function(y){
+        ES=sapply(l_niche,function(x){z=x[y,ES_col]})
         up_rank=sapply(rank_ls,function(x){z=x[y,'up_rank']})
         down_rank=sapply(rank_ls,function(x){z=x[y,'down_rank']})
-        CombRank(ES,up_rank,down_rank,min_num=min_num,min_ratio=min_ratio,method=method)
-    },USE.NAMES=TRUE) %>% t() %>% data.frame()   
+        CombRank(ES,up_rank,down_rank)
+    },mc.cores=ncores) %>% dplyr::bind_rows()
+
+    rownames(test_result)=gene_names
+    return(test_result)
+    
 }
