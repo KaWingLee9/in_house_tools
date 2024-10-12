7cb0c821c7f4eca6d7d614a91d73741ac05965d5
Kelvin Lee <lijiarong2020@sinh.ac.cn>
2024-10-12 13:13:05 +0800

    Update custom_fun.R

--- meta_analysis/custom_fun.R
+++ meta_analysis/custom_fun.R
@@ -139,13 +139,17 @@ CombRank_DFLs=function(l,p_col='p_val',ES_col='log_fc',
     },mc.cores=ncores)
     
     test_result=parallel::mclapply(gene_names,function(y){
-        ES=sapply(l_niche,function(x){z=x[y,ES_col]})
+        ES=sapply(l,function(x){z=x[y,ES_col]})
         up_rank=sapply(rank_ls,function(x){z=x[y,'up_rank']})
         down_rank=sapply(rank_ls,function(x){z=x[y,'down_rank']})
         CombRank(ES,up_rank,down_rank,min_num=min_num,min_ratio=min_ratio,method=method)
     },mc.cores=ncores) %>% dplyr::bind_rows()
-    rownames(test_result)=gene_names
+
+    test_result[,'median_pval']=parallel::mclapply(gene_names,function(y){
+        median( sapply(l,function(x){z=x[y,p_col]}) ,na.rm=TRUE)
+    },mc.cores=ncores)  %>% unlist()
     
+    rownames(test_result)=gene_names
     return(test_result)
     
 }
